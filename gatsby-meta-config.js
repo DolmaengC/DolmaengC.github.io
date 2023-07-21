378e40cc17b54fb731a6d607e268ada84880797f
ChoiJunhyeok00 <21900764@handong.ac.kr>
2023-07-22 03:10:55 +0900

    update for DNS

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `DolmaengC`,
+  description: `돌맹씨의 개발일기`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://dolmaengc.github.io/`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
